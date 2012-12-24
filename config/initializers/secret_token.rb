88a4bb17f22633e102881dfb6338d05eab7c6294
Lei Shi <wing831223@gmail.com>
2012-12-24 15:17:31 +0800

    Hello Dionysus

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dionysus::Application.config.secret_token = '8e780abebbcdb7c9bfd04b0343864c69436c4278b95263cbc9c45d1b76e6ea4d36d45abf9e48f30b2b765fbb39d009c4bdb14229d7d3653245e4801e4bb986c6'
---
+++ db/migrate/20121224071437_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :nick
+
+
+      t.timestamps
+    end
+  end
+end
